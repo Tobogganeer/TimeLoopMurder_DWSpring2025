9340644d9c56a9db89fee1eab6115d4804b6ded8
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-03-05 12:41:12 -0500

    Set up camera and cursors

--- Assets/Scripts/Props/ShowTextOnClick.cs
+++ Assets/Scripts/Props/ShowTextOnClick.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TextInteractable : MonoBehaviour, IInteractable
+public class ShowTextOnClick : MonoBehaviour, IInteractable
 {
     [TextArea(1, 3)]
     public string text;
