96d623ef4a64cbbb5598d988f9e2b3f508d042fb
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-03-06 14:40:37 -0500

    Made stopwatch scale and fill slowly

--- Assets/Scripts/UI/LoopStopwatch.cs
+++ Assets/Scripts/UI/LoopStopwatch.cs
@@ -8,12 +8,29 @@ public class LoopStopwatch : MonoBehaviour
     public Transform hand;
     public Image backgroundFill;
 
+    float bgFill;
+    int oldActionsLeft;
+
+    const float BGFillSpeed = 3f;
+    const float ScaleSpeed = 3f;
+    const float PopScaleSize = 0.1f;
+
     private void Update()
     {
+        bool justUsedAnAction = oldActionsLeft != Loop.ActionsLeft;
+        oldActionsLeft = Loop.ActionsLeft;
+
+        if (justUsedAnAction)
+            transform.localScale = Vector3.one * (PopScaleSize + transform.localScale.x);
+
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime * ScaleSpeed);
+
         // 0 when we have all actions left, 1 when we are finished
         float targetFac = 1f - Loop.PercentActionsLeft;
 
         hand.rotation = Quaternion.Euler(0, 0, targetFac * -360f);
-        backgroundFill.fillAmount = targetFac;
+
+        bgFill = Mathf.Lerp(bgFill, targetFac, Time.deltaTime * BGFillSpeed);
+        backgroundFill.fillAmount = bgFill;
     }
 }
