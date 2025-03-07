97dbb6df9e9acb3828a0f4eb629a92c86cdd1e28
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-03-06 22:32:08 -0500

    Fade in to game scene

--- Assets/Scripts/UI/GameSceneIntro.cs
+++ Assets/Scripts/UI/GameSceneIntro.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Tobo.Audio;
 
 public class GameSceneIntro : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image blackScreen;
+    public float fadeInTime = 1f;
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Start()
     {
-        
+        blackScreen.color = Color.black; // Set alpha to one
+        blackScreen.DOFade(0, fadeInTime);
+
+        yield return new WaitForSeconds(fadeInTime);
+        blackScreen.gameObject.SetActive(false); // Make sure we can interact through it
+        PopUp.Show("1 hour ago...", 2);
     }
 }
