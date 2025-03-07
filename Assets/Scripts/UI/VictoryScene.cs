b7b5c8ac0db947479678c4df80e14f5d45d75010
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-03-06 22:46:46 -0500

    Added dad and victory scene

--- Assets/Scripts/UI/VictoryScene.cs
+++ Assets/Scripts/UI/VictoryScene.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Tobo.Audio;
+using UnityEngine.SceneManagement;
 
 public class VictoryScene : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image blackScreen;
+    public float fadeInTime = 2f;
+
     void Start()
     {
-        
+        // TODO: Play sirens/paper sound?
+
+        blackScreen.color = Color.black; // Set alpha to one
+        blackScreen.DOFade(0, fadeInTime);
+
+        Sound.PhotoDrop.Play2D(); // Emulate paper sound for now
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReturnToMainMenu()
     {
-        
+        SceneManager.LoadScene("Main Menu");
     }
 }
